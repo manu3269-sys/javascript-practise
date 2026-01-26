b97c415e3d96c3738e68c6bdfb5ef9eea9ca746d
manu3269-sys <manur3269@gmail.com>
2026-01-26 18:04:52 +0530

    Initial commit: JS string methods practise

---
+++ 01_basics.js
@@ -0,0 +1,27 @@
+// string concept in js (type of string method )
+// let name = "javascript"
+// console.log(name.length) // tell the lenght of the string 
+
+
+// let name = "javascript"
+// console.log(name.charAt(5)) // accessing character 
+
+// let name = "javascript"
+// console.log(name.toUpperCase()) // capatalise all the character 
+
+// let name = "javascript"
+// console.log(name.lastIndexOf("a")) 
+
+// there are other method in string to like:-
+// .indexOf
+// .includes(print value in true or false)
+// .startWith(print value in true or false)
+// .endWith
+
+// let text = "javascript"
+// console.log(text.slice(0,4))
+
+
+const name = "Manu"
+const rollno = 62
+console.log(`hello my name is ${name} and my rollno is ${rollno}`)
